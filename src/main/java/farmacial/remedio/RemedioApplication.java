9ace01e7cd0d69c702e00917b4d778cca2404882
Janderson Borges Nogueira branco <jandinhoster@gmail.com>
2024-05-27 19:32:33 -0300

    spring boot

---
+++ src/main/java/farmacial/remedio/RemedioApplication.java
@@ -0,0 +1,13 @@
+package farmacial.remedio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemedioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemedioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/farmacial/remedio/RemedioApplicationTests.java
@@ -0,0 +1,13 @@
+package farmacial.remedio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemedioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
